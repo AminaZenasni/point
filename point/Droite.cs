2754ee84d0c944bb066a86c6e6a63a053807f942
AminaZenasni <116212206+AminaZenasni@users.noreply.github.com>
2022-11-11 00:01:19 +0100

    Update Droite.cs

--- point/Droite.cs
+++ point/Droite.cs
@@ -8,5 +8,30 @@ namespace point
 {
     internal class Droite
     {
+         private point p ;
+        private point q; 
+
+        public droite(point p, point q)
+        {
+            this.p = p;
+            this.q = q;
+        }
+
+        public point Getp()
+        {
+            return p;
+        }
+        public point Getq()
+        {
+            return q;
+        }
+        public void Setp(point p)
+        {
+            this.p = p;
+        }
+        public void Setq(point q)
+        {
+            this.q = q;
+        }
     }
 }
